452ad0fc090dcc464c796c0881ccf6e9020dbf8d
yangyida1 <motuolluo02@163.com>
2023-06-21 11:53:24 +0800

    默认更改列表

---
+++ src/main/java/com/yunjia/bean/UserInfo.java
@@ -0,0 +1,20 @@
+package com.yunjia.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserInfo {
+    private String  uid;
+    private int buttonId ;
+    private Boolean result;
+
+}
